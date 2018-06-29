193f937f157fe01498fdc68f8ddb0f4eba413fc2
Mohamed Ahmed <error17191@gmail.com>
2018-06-29 13:59:47 +0200

    Undo Functionality

--- public/app.js
+++ public/app.js
@@ -4,14 +4,33 @@ var $filmName = document.getElementById('film-name');
 
 var $year = document.getElementById('film-year');
 
+var $undoBtn = document.querySelector('.undo');
+
+var lastFilm, savedFilm;
+
 $mainBtn.addEventListener('click', function (e) {
     e.preventDefault();
+    lastFilm = savedFilm;
     $mainBtn.classList.add('loading');
     axios.get('app.php').then(function (response) {
         $mainBtn.classList.remove('loading');
         var film = response.data;
+        savedFilm = film;
+        if (lastFilm) {
+            $undoBtn.style.visibility = 'visible';
+        }
+
         $filmName.textContent = film.name;
         $filmName.href = film.link;
         $year.textContent = film.year;
     });
+});
+
+$undoBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    $filmName.textContent = lastFilm.name;
+    $filmName.href = lastFilm.link;
+    $year.textContent = lastFilm.year;
+    savedFilm = lastFilm;
+    $undoBtn.style.visibility = 'hidden';
 });
