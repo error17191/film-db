8f9a4f7001648f8dc8876a0c3c37ed8e9fba4337
Mohamed Ahmed <error17191@gmail.com>
2018-07-25 18:33:41 +0200

    Used Vue

---
+++ public/app-003.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+    el: '#app',
+    mounted() {
+        this.ready = true;
+    },
+    data: {
+        ready:false,
+        lastFilm: null,
+        currentFilm: null,
+        loadingFilm: false,
+        undoVisibility: 'hidden',
+        showReporting: false,
+        showReported: false,
+    },
+    methods: {
+        clickedOnMainButton() {
+            this.lastFilm = this.currentFilm;
+            this.loadingFilm = true;
+            axios.get('app.php').then(response => {
+                this.showReporting = false;
+                this.showReported = false;
+                this.loadingFilm = false;
+                this.currentFilm = response.data;
+
+                if (this.lastFilm) {
+                    this.undoVisibility = 'visible';
+                }
+
+            });
+        },
+        report() {
+            this.showReporting = true;
+            axios.post('report.php', {film: this.currentFilm.uid}).then(response => {
+                if (response.data.success) {
+                    this.showReporting = false;
+                    this.showReported = true;
+                }
+            });
+        },
+        undo() {
+            this.currentFilm = this.lastFilm;
+            this.undoVisibility = 'hidden';
+        }
+    }
+});
